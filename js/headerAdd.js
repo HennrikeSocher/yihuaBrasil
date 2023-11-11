35001b141085507c9597a1d22e4717b424962770
paocomeggs <ti.budziak@gmail.com>
2023-11-10 22:59:43 -0300

    ajuste append header, ajuste carrossel

---
+++ js/headerAdd.js
@@ -0,0 +1,18 @@
+$("header").append('<a href="./index.html" style="text-decoration: none; color: "#fff" "><p>Yihua<br>Brasil</p></a>');
+
+$("header").append('<div class="navbar">')
+
+$(".navbar").append('<a href="./index.html"><p class="link-text">Inicio</p></a>')
+$(".navbar").append('<a href="./sobrenos.html"><p class="link-text">Sobre Nos</p></a>')
+$(".navbar").append('<a href="./servicos.html"><p class="link-text">Servicos</p></a>')
+$(".navbar").append('<a href="./produtos.html"><p class="link-text">Produtos</p></a>')
+$(".navbar").append('<a href="./contato.html"><p class="link-text">Contato </p></a>')
+
+$("header").append('</div>');
+
+$("header").append('<div class="links">')
+
+$(".links").append('<a href="instagram.com"><i class="fa-brands fa-instagram headerIcon"></i></a>')
+$(".links").append('<a href="whatsapp"><i class="fa-brands fa-whatsapp headerIcon"></i></a>')
+
+$("header").append('</div>');
